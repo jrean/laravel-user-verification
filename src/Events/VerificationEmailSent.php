5b01ac2ee1e482d0798695c78257fff6b8fac838
Lukas Oppermann <lukas@vea.re>
2016-09-10 21:33:54 +0200

    Fire VerificationEmailSent Event

---
+++ src/Events/VerificationEmailSent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Jrean\UserVerification\Events;
+
+use Illuminate\Queue\SerializesModels;
+
+class VerificationEmailSent
+{
+    use SerializesModels;
+
+    /**
+     * The authenticated user.
+     *
+     * @var \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+}
