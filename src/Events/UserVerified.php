ec15e99fc971cac8f552a9b8e6bf7df340fa1f91
yarob <yarob.al-taay@union.co.uk>
2017-03-15 15:50:23 +0000

    Add "Verified" event on successful user verification

---
+++ src/Events/UserVerified.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of Jrean\UserVerification package.
+ *
+ * (c) Jean Ragouin <go@askjong.com> <www.askjong.com>
+ */
+namespace Jrean\UserVerification\Events;
+
+use Illuminate\Queue\SerializesModels;
+
+class UserVerified
+{
+    use SerializesModels;
+
+    /**
+     * The authenticated user.
+     *
+     * @var \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+}
