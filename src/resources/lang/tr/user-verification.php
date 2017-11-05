f78e542df5c70b9e9d5ce72e1d35b197bbfe3023
Mehmet Hanoğlu <mhanoglu55@gmail.com>
2017-11-05 21:41:14 +0300

    Turkish language file

---
+++ src/resources/lang/tr/user-verification.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel User Verification Language Pack
+    |--------------------------------------------------------------------------
+    */
+
+    'verification_email_subject' => 'Hesap Doğrulama Linkiniz',
+
+    // error view
+    'verification_error_header'      => 'Doğrulama başarısız',
+    'verification_error_message'     => 'Hesabınız onaylanamıyor.',
+    'verification_error_back_button' => 'Geri',
+
+];
