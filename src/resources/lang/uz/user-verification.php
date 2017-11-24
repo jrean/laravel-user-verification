b404e9655d7a02137a05d9b08e1b3aa8c386c7d1
Inoyatulloh <mwapftp@gmail.com>
2017-11-25 03:38:21 +0500

    Update user-verification.php

--- src/resources/lang/uz/user-verification.php
+++ src/resources/lang/uz/user-verification.php
@@ -8,6 +8,6 @@ return [
     'verification_email_subject' => 'Hisobni faollashtirish',
     // error view
     'verification_error_header' => 'Faollashtirish amalga oshmadi',
-    'verification_error_message' => 'Hisobingizni faollashtirib bo`lmaydi.',
+    'verification_error_message' => 'Hisobingizni faollashtirib bo`lmadi.',
     'verification_error_back_button' => 'Orqaga',
 ];
