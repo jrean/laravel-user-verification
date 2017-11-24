c098cc5d93c5db97ad72a4e7db871f51e8938925
Inoyatulloh <mwapftp@gmail.com>
2017-11-25 03:35:36 +0500

    Adding Uzbek translation

---
+++ src/resources/lang/uz/user-verification.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel User Verification Language Pack
+    |--------------------------------------------------------------------------
+    */
+    'verification_email_subject' => 'Hisobni faollashtirish',
+    // error view
+    'verification_error_header' => 'Faollashtirish amalga oshmadi',
+    'verification_error_message' => 'Hisobingizni faollashtirib bo`lmaydi.',
+    'verification_error_back_button' => 'Orqaga',
+];
