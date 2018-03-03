d9f5bf7ea261b4d54abc6dbc890a95a7d81338c4
Andrés Herrera García <andreshg112@gmail.com>
2018-03-02 23:46:33 -0500

    1. Redirect to a view when the user is verified. 2. Added mostUpdate (array) attribute when some fiels also need to be updated in the table.

--- src/resources/lang/en/user-verification.php
+++ src/resources/lang/en/user-verification.php
@@ -6,7 +6,7 @@ return [
     |--------------------------------------------------------------------------
     | Laravel User Verification Language Pack
     |--------------------------------------------------------------------------
-    */
+     */
 
     'verification_email_subject' => 'Account Verification',
 
@@ -14,5 +14,15 @@ return [
     'verification_error_header' => 'Verification failed',
     'verification_error_message' => 'Your account could not be verified.',
     'verification_error_back_button' => 'Back',
+
+    // is verified view
+    'verification_verified_header' => ':name, your account has been previously verified.',
+    'verification_verified_message' => 'Thank you for using our services!',
+    'verification_verified_back_button' => 'Back',
+
+    // after verification view
+    'verification_verification_header' => ':name, your account has been verified correctly.',
+    'verification_verification_message' => 'Thank you for using our services!',
+    'verification_verification_back_button' => 'Back',
 
 ];
