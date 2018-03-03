b7a9421d9c9dab995f4face7c7467f38fdfb7a5c
Andrés Herrera García <andreshg112@gmail.com>
2018-03-02 23:48:44 -0500

    Added spanish lang.

---
+++ src/resources/lang/es/user-verification.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel User Verification Language Pack
+    |--------------------------------------------------------------------------
+     */
+
+    'verification_email_subject' => 'Verificación de la cuenta',
+
+    // error view
+    'verification_error_header' => 'Fallo en la verificación',
+    'verification_error_message' => 'Su cuenta no pudo ser verificada.',
+    'verification_error_back_button' => 'Atrás',
+
+    // is verified view
+    'verification_verified_header' => ':name, su cuenta ya ha sido verificada anteriormente.',
+    'verification_verified_message' => '¡Gracias por utilizar nuestros servicios!',
+    'verification_verified_back_button' => 'Atrás',
+
+    // after verification view
+    'verification_verification_header' => ':name, su cuenta ha sido verificada correctamente.',
+    'verification_verification_message' => '¡Gracias por utilizar nuestros servicios!',
+    'verification_verification_back_button' => 'Atrás',
+];
